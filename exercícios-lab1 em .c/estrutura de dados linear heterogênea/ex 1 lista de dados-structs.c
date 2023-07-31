45317aaf1414567b93f91697c037870af366c394
Claudio André Ferreria Caron <140202178+Claudio-Caron@users.noreply.github.com>
2023-07-30 21:13:09 -0400

    exercício incompleto

--- exercícios-lab1 em .c/estrutura de dados linear heterogênea/ex 1 lista de dados-structs.c
+++ exercícios-lab1 em .c/estrutura de dados linear heterogênea/ex 1 lista de dados-structs.c
@@ -14,8 +14,28 @@ int main (){
     typedef struct tipo_pessoa tipo_pessoa;
     tipo_pessoa pessoa[espaco];
     int i, x;
-    for (i=0, i<3, i++){
-        
+    puts ("-----------insira os dados----------\n")
+    for (i=0; i<3; i++){
+        printf ("nome (%d)\n", i+1);
+        fgets (nome[i], tam, stdin);
+        fflush (stdin);
+
+        puts (idade: );
+        scanf ("%d", idade[i]);
+        fflush (stdin);
+
+        puts (altura em cm: );
+        scanf ("%f", altura[i]);
+        fflush (stdin);
     }
+    system (cls);
+    puts ("___---dados inseridos das respectias pessoas---___");
+    for (i=0; i<3; i++)
+        printf ("pessoa %i: %s\nidade: %d anos\n altura %f metros\n", i+1, nome[i], idade[i], altura[i]/100);
+    
+
+
+    system ("pause");
+    return 0;
 
 }
