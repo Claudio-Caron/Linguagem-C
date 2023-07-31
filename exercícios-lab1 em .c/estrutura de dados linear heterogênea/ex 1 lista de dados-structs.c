ee49835dc881b24e7e1d49ff98d9865032f6dbc1
Claudio André Ferreria Caron <140202178+Claudio-Caron@users.noreply.github.com>
2023-07-31 17:15:01 -0400

    exercício do formulário encerrado

--- exercícios-lab1 em .c/estrutura de dados linear heterogênea/ex 1 lista de dados-structs.c
+++ exercícios-lab1 em .c/estrutura de dados linear heterogênea/ex 1 lista de dados-structs.c
@@ -2,39 +2,73 @@
 #include <stdlib.h>
 #include <string.h>
 #include <locale.h>
-#define tam 50
-#define espaco 3
+#include <ctype.h>
+//solicitar um formulário com N pessoas, solicitando nome, peso, altura, idade e sexo. Mostrar os dados digitados
+#define N 1
 struct tipo_pessoa {
     int idade;
     float altura;
-    char nome[tam];
+    float peso;
+    char nome[50];
+    char sexo;
 };
+typedef struct tipo_pessoa tipo_pessoa;
 int main (){
     setlocale (LC_ALL, "Portuguese");
-    typedef struct tipo_pessoa tipo_pessoa;
-    tipo_pessoa pessoa[espaco];
-    int i, x;
-    puts ("-----------insira os dados----------\n")
-    for (i=0; i<3; i++){
-        printf ("nome (%d)\n", i+1);
-        fgets (nome[i], tam, stdin);
+    tipo_pessoa form[N];
+    int i;
+    /*int x;*/
+    puts ("\t\t\t\t\t-----------Formulário pessoal----------\n\n");
+    for (i=0; i<N; i++){
+        printf ("\tDados da pessoa (%i)\n------------------------------------\n", i+1);
+        puts ("nome : ");
+        fgets (form[i].nome, 50, stdin);
+        do{
+        puts ("Sexo: (M ou F)");
+        scanf ("%c", &form[i].sexo);
+        fflush (stdin);
+        if (form[i].sexo!='f' && form[i].sexo!='F' && form[i].sexo!='m' && form[i].sexo!='M'){
+            printf ("dado inserido: %c\n", form[i].sexo);
+            puts ("insira novamente, dessa vez, opte somente feminino ou masculino, f ou m");   
+        }
+        }while (form[i].sexo!='f' && form[i].sexo!='F' && form[i].sexo!='m' && form[i].sexo!='M');
+
+        puts ("Idade:  ");
+        scanf ("%d", &form[i].idade);
         fflush (stdin);
 
-        puts (idade:  );
-        scanf ("%d", idade[i]);
+        puts ("Altura em cm: ");
+        scanf ("%f", &form[i].altura);
         fflush (stdin);
 
-        puts (altura em cm: );
-        scanf ("%f", altura[i]);
+        puts ("Peso em KG: ");
+        scanf ("%f", &form[i].peso);
         fflush (stdin);
+        system ("cls");
+       /*printf ("Para continuar, insira 1, para preencher novamente os dados da pessoa, 2\n");
+        scanf ("%i", &x);
+        switch (x){
+            case 1:
+                continue;
+            case 2:
+                printf ("Opção escoçhida %i\nSolicitando novamente os dados da pessoa %i:\n");
+                i--;
+                continue;
+            default:
+                printf ("Insira uma opção correta, apenas 1 ou 2\n");
+                printf ("Para continuar, insira 1, para preencher novamente os dados da pessoa, 2\n");
+                scanf ("%i", &x);
+                // a versão com switch não funciona por enquanto, pois se fosse utilizado o loop para garantir a veracidade da resposta, o "continue" da opção 1 ou 2 iria retornar ao loop do while do switch e nunca ao for;
+        }*/
     }
-    system (cls);
-    puts ("___---dados inseridos das respectias pessoas---___");
-    for (i=0; i<3; i++)
-        printf ("pessoa %i: %s\nidade: %d anos\n altura %f metros\n", i+1, nome[i], idade[i], altura[i]/100);
-    
-
-
+    puts ("---------dados inseridos das respectivas pessoas---------\n");
+    for (i=0; i<N; i++){
+        printf ("-------Preenchimento da pessoa %i-------\n", i+1);
+        puts ("_______________________________________");
+        printf ("\tNome  : %s\n\tIdade : %d anos\n\tAltura: %.2f Metros\n", form[i].nome, form[i].idade, form[i].altura/100);
+        printf ("\tPeso  : %.2f KG\n\tSexo  : %c\n\n", form[i].peso, form[i].sexo);
+        puts ("\n");
+    }
     system ("pause");
     return 0;
 
