a7a1ef6a1ab4dc5c4b0984c7f23848bfe134da71
Claudio André Ferreria Caron <140202178+Claudio-Caron@users.noreply.github.com>
2023-08-03 11:48:00 -0400

    futuro projeto
    adicionado ideia de exercício bolado por mim , porém, minhas ideias estão à frente do que estou aprendendo no bimestre, devo focar mais em exercícios pré-determinados pelo professor!!!

--- exercícios-lab1 em .c/estrutura de dados linear heterogênea/lista de contatos.c
+++ exercícios-lab1 em .c/estrutura de dados linear heterogênea/lista de contatos.c
@@ -3,4 +3,5 @@
 #include <stdlib.h>
 #include <string.h>
 #include <locale.h>
-
+#include <ctype.h>
+//ideia: fazzer um struct de matriz, struct para armazenar as informações do contato e matriz para permitir a busca em linha com letras iniciais(usar toupper para trasformar a primeira posição do vetor em maiúscula, e para buscar, comparar letras do alfabeto inteiro com strcmp para buscar a letra dentro do laço, ou usar ponteiro para apontar a posição de memória do vetor/usar o cls após cada cadastro de contato para limpar o out) e coluna para armazenar o número respectivo da letra, para manter a lista em ordem alfabética. Futuramente, de alguma maneira, criar um arquivo txt para manter a lista armazenada na máquina, para somente importá-la, adicionando novos contatos a cada execução.
