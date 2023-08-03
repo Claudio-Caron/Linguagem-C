ade4260f3d4c5ba32936bc6ae8c454743b6c0265
Claudio André Ferreria Caron <140202178+Claudio-Caron@users.noreply.github.com>
2023-08-03 17:31:42 -0400

    atualização no exercicio
    foi adicionado um procedimento com objetivo de, por meio de desvio de fluxo,      fazer com que o usuário digite valores válidos. Função utilizada duas vezes no código

--- exercícios-lab1 em .c/Funções/Ex 2 média e situação-aluno.c
+++ exercícios-lab1 em .c/Funções/Ex 2 média e situação-aluno.c
@@ -4,22 +4,25 @@
 média, e mostrar a situação do aluno (Aprovado para média >=6 ou 
 Reprovado, caso contrário). Use modularidade*/
 void media (float x, float y);
-float z;
+void mensagem(float t);
 int main (){
     float a, b;
     do {
     puts ("insira a nota 1");
     scanf ("%f", &a);
+    mensagem (a);
     }while (a<0 || a>10);
     do{
     puts ("insira a nota 2");
     scanf ("%f", &b);
+    mensagem (b);
     }while (b<0 || b>10);
     media (a,b);
     system ("pause");
     return 0;
 }
 void media (float x, float y){
+    float z;
     z= (x+y)/2;
     printf ("A media do aluno : %.2f\n", z);
     if (z>=6)
@@ -27,4 +30,7 @@ void media (float x, float y){
     else 
         puts ("Com essa nota, o aluno esta reprovado");
 }
-
+void mensagem (float t){
+    if (t<0 || t>10)
+        puts ("nota invalida! Insira um nota entre 0 e 10");
+}
