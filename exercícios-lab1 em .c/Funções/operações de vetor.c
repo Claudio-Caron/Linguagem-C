30a19509133b00d9af6780d6b86074b9c1b226c2
Claudio André Ferreria Caron <140202178+Claudio-Caron@users.noreply.github.com>
2023-08-03 21:14:25 -0400

    novo plano de exercício

---
+++ exercícios-lab1 em .c/Funções/operações de vetor.c
@@ -0,0 +1,7 @@
+/*2. Operações sobre vetor. Declare, na região global, um vetor de inteiros de 
+5 posições. 
+O usuário deve informar uma das seguintes opções: 
+1. Inserir 2. Imprimir 3. Sair. 
+Exiba este menu enquanto a opção for diferente de 3. 
+Ambas opções, 2 e 3, devem imprimir os elementos do vetor. 
+Faça uso de laço do-while e de switch-case.*/
