bdd57aebf43133ca24526fa5e9c0a55054ddcb26
Claudio André Ferreria Caron <140202178+Claudio-Caron@users.noreply.github.com>
2023-08-10 16:19:52 -0400

    exercício novo

--- exercícios-lab1 em .c/Funções/ex 3 funcao-saque.c
+++ exercícios-lab1 em .c/Funções/ex 3 funcao-saque.c
@@ -1,18 +1,18 @@
-/*3.Faça uma solução usando modularidade para automatizar um sistema 
-bancário de um caixa eletrônico. O menu a seguir deve ser exibido ao 
-usuário enquanto a opção informada por ele seja diferente de SAIR: 
+/*3.Faça uma solução usando modularidade para automatizar um sistema
+bancário de um caixa eletrônico. O menu a seguir deve ser exibido ao
+usuário enquanto a opção informada por ele seja diferente de SAIR:
 1.SALDO 2.SAQUE 3.PAGAMENTO 4.DEPOSITO 0.SAIR
-Para cada opção, mostre o saldo no início, e informe ao usuário se a 
+Para cada opção, mostre o saldo no início, e informe ao usuário se a
 operação escolhida foi realizada com sucesso ou não.
 Considere saldo inicial igual $1500*/
-/*O sistema deve ser consistente: não permitir saque sem ter saldo; não permitir sacar, 
+/*O sistema deve ser consistente: não permitir saque sem ter saldo; não permitir sacar,
 depositar ou pagar valor menor ou igual a zero...
 Teste cada módulo antes de definir todos eles*/
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h> 
-// um modulo para cada opção, de maneira análoga ao exercício anterior(um switch com funções em cada caso). 
-float saldo=1500;
+#include <locale.h>
+// um modulo para cada opção, de maneira análoga ao exercício anterior(um switch com funções em cada caso).
+float saldo = 1500;
 void c1();
 void c2();
 void c3();
@@ -20,103 +20,126 @@ void c4();
 void men1();
 void men2();
 void escolha(int x);
-int main (){
+int main()
+{
     int a;
-    setlocale (LC_ALL, "Portuguese");
-    do{
-    puts ("*SISTEMA DE CAIXA ELETRÔNICO*");
-    puts ("Qual opção deseja realizar?");
-    printf ("1.SALDO\n2.SAQUE\n3.PAGAMENTO\n4.DEPOSITO\n0.SAIR\n");
-    scanf ("%d", &a);
-    system ("cls");
-    printf ("Opcão escolhida :%d\n", a);
-    if (a>4 || a<0){
-        printf ("Escolha somente uma das opções disponíveis!!!\n");
-        continue;
-    }
-    escolha (a);
-    }while (a!=0);
+    setlocale(LC_ALL, "Portuguese");
+    do
+    {
+        printf ("\t\t\t\t\t*SISTEMA DE CAIXA ELETRÔNICO*\n");
+        printf ("\tQual opção deseja realizar?\n");
+        printf("\t1.SALDO\n\t2.SAQUE\n\t3.PAGAMENTO\n\t4.DEPOSITO\n\t0.SAIR\n");
+        scanf("%d", &a);
+        system("\tcls");
+        printf("\tOpcão escolhida :%d\n", a);
+        if (a > 4 || a < 0)
+        {
+            printf("\tEscolha somente uma das opções disponíveis!!!\n");
+            continue;
+        }
+        escolha(a);
+    } while (a != 0);
+    system("pause");
+    return 0;
 }
-void c1(){
-    printf ("Saldo atual : %.2f\n", saldo);
+void c1()
+{
+    printf("\tSaldo atual : %.2f\n", saldo);
 }
-void c2(){
+void c2()
+{
     float y;
     c1();
-    puts ("2-SAQUE");
-    puts ("Qual valor deseja sacar?");
-    puts ("(para cancelar o saque, digite 0)");
-    scanf ("%f", &y);
-    if (y==0){
+    printf("\t2-SAQUE\n");
+    printf("\tQual valor deseja sacar?\n");
+    printf("\t(para cancelar o saque, digite 0)\n");
+    scanf("%f", &y);
+    if (y == 0)
+    {
         men2();
     }
-    while (y!=0){
-    if (y<=saldo){
-        saldo -=y;
-        men1();
-    }else{
-        printf ("O valor inserido excede seu saldo atual, para efetuar o saque, digite um valor correspondente ao seu saldo atual;\n");
-        men2();
-    }
-    y=0;
-    }
-}
-void c3(){
-    float y;
-    c1();
-    puts ("3-PAGAMENTO");
-    puts ("Insira o valor da conta que deseja pagar");
-    scanf ("%f", &y);
-    if (y<=saldo){
-        saldo -=y;
-        men1();
-    }else{
-        printf ("O valor inserido excede seu saldo atual, não é possível realizar o pagamento\n");
-        men2 ();
-    }
-}
-void c4(){
-    float y;
-    c1();
-    puts ("4-DEPÓSITO");
-    do{
-    puts ("insira o valor para depósito:");
-    puts ("(para cancelar a operação, digite 0)");
-    scanf ("%f", &y);
-    if (y<0)
-        puts ("Insira um valor válido para depósito");
-    }while (y<0);
-    saldo +=y;
-    if (y>0){
-        men1();
-    }else{
-        men2();
-    } 
-
-}
-
-void men1(){
-        puts ("Operação realizada com sucesso!\n");
-}
-void men2(){
-        printf ("Operação não realizada, seu saldo atual é de : RS %.2f\n", saldo);
-}
-void escolha (int x){
-    switch (x){
-        case 1:
-            puts ("1-SALDO");
+    while (y != 0)
+    {
+        if (y <= saldo)
+        {
+            saldo -= y;
             men1();
-            c1();
-            break;
-        case 2:
-            c2();
-            break;
-        case 3:
-            c3();
-            break;
-        case 4:
-            c4();
-            break;
+        }
+        else
+        {
+            printf("\tO valor inserido excede seu saldo atual\n\tPara efetuar o saque, digite um valor correspondente ao seu saldo atual;\n");
+            men2();
+        }
+        y = 0;
+    }
+}
+void c3()
+{
+    float y;
+    c1();
+    printf ("\t3-PAGAMENTO\n");
+    printf ("\tInsira o valor da conta que deseja pagar\n");
+    scanf("%f", &y);
+    if (y <= saldo)
+    {
+        saldo -= y;
+        men1();
+    }
+    else
+    {
+        printf("\tO valor inserido excede seu saldo atual, não é possível realizar o pagamento\n");
+        men2();
+    }
+}
+void c4()
+{
+    float y;
+    c1();
+    printf ("\t4-DEPÓSITO\n");
+    do
+    {
+        printf ("\tinsira o valor para depósito:\n");
+        printf ("\t(para cancelar a operação, digite 0)\n");
+        scanf("%f", &y);
+        if (y < 0)
+            printf ("\tInsira um valor válido para depósito\n");
+    } while (y < 0);
+    saldo += y;
+    if (y > 0)
+    {
+        men1();
+    }
+    else
+    {
+        men2();
     }
 }
 
+void men1()
+{
+    printf("\tOperação realizada com sucesso!\n\n");
+}
+void men2()
+{
+    printf("\tOperação não realizada, seu saldo atual é de : RS %.2f\n", saldo);
+}
+void escolha(int x)
+{
+    switch (x)
+    {
+    case 1:
+        printf ("\t1-SALDO\n");
+        men1();
+        c1();
+        break;
+    case 2:
+        c2();
+        break;
+    case 3:
+        c3();
+        break;
+    case 4:
+        c4();
+        break;
+    }
+}
---
+++ exercícios-lab1 em .c/ponteiros.c/ex 1 ponteiros.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/*Faça uma solução, usando modularidade, para o sistema armazenar 10 inteiros 
+em um vetor. O usuário poderá escolher, dentre as opções:
+[D]uplicar: 
+Todos os elementos do vetor são duplicados. Imprima-os, em seguida 
+[I]mprimir:
+Todos os elementos do vetor são impressos 
+[R]emover: 
+O último elemento do vetor deve ser removido. Imprima-os, em seguida
+[S]air
+Todos os elementos do vetor são impressos e o sistema é encerrado
+Obs.: Todo vetor é passado por referência. Logo, qualquer modificação nesta 
+estrutura dentro de um módulo não requer o seu retorno.
+Para esta tarefa, declare o vetor na função main()*/
+//void armvetor(int *);
+void imprimir (int *);
+void armvetor (int *);
+int main (){
+    int a;
+    int vet[10]={1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    //int *pvet;
+    //int pvet=&vet[10];
+    do {
+        printf ("Escolha uma opcao\n[D]uplicar\n[I]mprimir\n[R]emover\n[S]air\n");
+        scanf ("%c", &a);
+        toupper ('a');
+        switch (a) {
+            case 'D':
+                printf ("opcao escolhida: DUPLICAR");
+                //
+                break;
+            case 'I':
+                printf ("opcao escolhida: IMPRIMIR");
+                imprimir(vet);
+                break;
+            case 'R':
+                printf ("opcao escolhida: REMOVER");
+                //
+                break;
+            case 'S':
+                printf ("opcao escolhida: SAIR\n");
+                imprimir(vet);
+                break;
+        }
+    }while(a!='S');
+
+    system ("pause");
+    return 0;
+}
+void armvetor(int *pvet){
+    int i;
+    for (i=0; i<10; i++){
+        printf ("insira o valor da posicao %i do vetor\n", i+1);
+        scanf ("%i", &pvet[i]);
+        pvet[i]= pvet[i] * 2;
+    }
+    imprimir (pvet);
+}
+void imprimir(int *pvet){
+    int i;
+    printf ("Vetor: ");
+    for(i=0; i<10; i++){
+        printf ("%i ", pvet[i]);
+    }
+    printf ("\n");
+}
+//void escolha (){
+    printf ("Escolha uma opcao\n[D]uplicar\n[I]mprimir\n[R]emover\n[S]air\n");
+    scanf ("%c", &a);
+    toupper ('a');
+    switch (a) {
+        case 1:
+            //
+            break;
+        case 2:
+            imprimir()
+
+    }
+}
