082f12d30613ded8df1ac34dc97240d5c92e9804
Claudio André Ferreria Caron <140202178+Claudio-Caron@users.noreply.github.com>
2023-08-14 20:33:08 -0400

    adicionado exercicio e pasta

---
+++ exercícios-lab1 em .c/ponteiros.c/jogo/milão da maromba.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+#include <ctype.h>
+void show();
+void pergunta();
+void escolha();
+    void p1(char *, int *);
+    void p2(char *, int *);
+    void p3(char *, int *);
+    void p4(char *, int *);
+    void p5(char *, int *);
+    void p6(char *, int *);
+    void p7(char *, int *);
+    void p9(char *, int *);
+int main (){
+    setlocale (LC_ALL,"Portuguese");
+    int x, y;
+    mensagem ();
+
+}
+void mensagem(){
+    puts ("\t\t\t=======||  ||       || //=======\\  \\               //");
+    puts ("\t\t\t||         ||       || ||       ||  \\             //");
+    puts ("\t\t\t||======|| ||=======|| ||       ||   \\    //\\    //");
+    puts ("\t\t\t        || ||       || ||       ||    \\  //  \\  //");
+    puts ("\t\t\t||======== ||       ||  \\=======//     \\//    \\//");
+    puts ("\n\t\t\t\t\t||==\\   /====\\");
+    puts ("\t\t\t\t\t||  |   |    |");
+    puts ("\t\t\t\t\t||==/   \\====/");
+    puts ("\t\t\t");
+    puts ("\t\t\t");
+    puts ("\t\t\t");
+    puts ("\t\t\t");
+    puts ("\t\t\t");
+    puts ("\t\t\t");
+    puts ("\t\t\t\t\t\t\t\tDA MAROMBA\n");
+
+}
+void escolha(){
+    do {
+        int cont=0, rodada=1;
+        switch{
+            case 1:
+                mensrodada(rodada);
+                prit
+        }
+        if (x!=op){//x vai ser a resposta e op a opçao escolhida como resposta
+            puts ("Alternativa incorreta\nOpção correta:%d\nVocê encerrou o jogo com // reais");
+            cont++;
+        }
+        rodada++;
+    }while (cont==0 || rodada<=10); 
+}
+void mensrodada(int esprodada){
+    printf ("\t\t\tRodada %d, Responda a pergunta corretamente e acumule prêmios!\n", esprodada);
+}
