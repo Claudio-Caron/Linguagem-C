85ffb2c360b29da58fa0037b1646d319dd96aa48
Claudio André Ferreria Caron <140202178+Claudio-Caron@users.noreply.github.com>
2023-08-15 14:16:03 -0400

    melhorias

--- exercícios-lab1 em .c/ponteiros.c/jogo/cabeçalho do mhilão.c
+++ exercícios-lab1 em .c/ponteiros.c/jogo/cabeçalho do mhilão.c
@@ -1,27 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <locale.h>
-#include <string.h>
-#include <ctype.h>
-#include <time.h>//rand()
-void show();
-void pergunta();
-void escolha();
-    void p1(char *, int *);
-    void p2(char *, int *);
-    void p3(char *, int *);
-    void p4(char *, int *);
-    void p5(char *, int *);
-    void p6(char *, int *);
-    void p7(char *, int *);
-    void p9(char *, int *);
-int main (){
-    setlocale (LC_ALL,"Portuguese");
-    int x, y;
-    mensagem ();
-
-}
-void mensagem(){
+#include <windows.h>
+int main(){
     int j;
     puts ("\t\t\t=======||  ||       || //=======\\  \\\\          //");
     puts ("\t\t\t||         ||       || ||       ||  \\\\        //");
@@ -48,26 +27,6 @@ void mensagem(){
         Sleep (1000);
     }
     printf (" \n");
-}
-void escolha(){
-    do {
-        int cont=0, rodada=1;
-        switch{
-            case 1:
-                mensrodada(rodada);
-                
-        }
-        if (x!=op){//x vai ser a resposta e op a opçao escolhida como resposta
-            puts ("Alternativa incorreta\nOpção correta:%d\nVocê encerrou o jogo com // reais");
-            cont++;
-        }
-        rodada++;
-    }while (cont==0 || rodada<=10); 
-}
-void mensrodada(int esprodada){
-    printf ("\t\t\tRodada %d, Responda a pergunta corretamente e acumule prêmios!\n", esprodada);
-}
-    int p1(char *, int *);
-
-
+    
     return 0;
+}
