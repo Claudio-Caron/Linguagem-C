3be951c1c0c37a17c0b284327e7a8371910718f5
Claudio André Ferreria Caron <140202178+Claudio-Caron@users.noreply.github.com>
2023-08-16 11:09:04 -0400

    alteracao no jogo e novo exercício de parametro

--- exercícios-lab1 em .c/ponteiros.c/jogo/milão da maromba.c
+++ exercícios-lab1 em .c/ponteiros.c/jogo/milão da maromba.c
@@ -11,15 +11,15 @@ void preencher();
 void pergunta();
 void escolha();
 void certo();
-void p1(char *, int *);
-void p2(char *, int *);
+int p1(char gaba, int x);
+/*void p2(char *, int *);
 void p3(char *, int *);
 void p4(char *, int *);
 void p5(char *, int *);
 void p6(char *, int *);
 void p7(char *, int *);
 void p9(char *, int *);
-int p10(char *, int *);
+int p10(char *, int *);*/
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
@@ -62,7 +62,9 @@ void mensagem()
 }
 void escolha()
 {
-    char gabarito[11]="BBCDCDDDDC";
+    int cont = 0, rodada = 0;
+    char gabarito[11] = "BBCDCDDDDC";
+    char premios[11][]
     /*1- B
       2- B
       3- C
@@ -75,42 +77,41 @@ void escolha()
       10-C*/
     do
     {
-        int cont = 0, rodada = 1;
         switch (rodada)
         {
+        case 0: // inicia rodada 1, porém, utilizei o 0 para reciclar a variavel em outra função
+            cont = p1(gabarito[0], rodada);
+            break;
         case 1:
-            cont = p1(gabarito[0]);
+            cont = p1(gabarito[1], rodada);
             break;
         case 2:
-            cont = p2(gabarito[1]);
+            cont = p1(gabarito[2], rodada);
             break;
         case 3:
-            cont = p3(gabarito[2]);
+            cont = p1(gabarito[3], rodada);
             break;
         case 4:
-            cont = p4(gabarito[3]);
+            cont = p1(gabarito[4], rodada);
             break;
         case 5:
-            cont = p5(gabarito[4]);
+            cont = p1(gabarito[5], rodada);
             break;
         case 6:
-            cont = p6(gabarito[5]);
+            cont = p1(gabarito[6], rodada);
             break;
         case 7:
-            cont = p7(gabarito[6]);
+            cont = p1(gabarito[7], rodada);
             break;
         case 8:
-            cont = p8(gabarito[7]);
+            cont = p1(gabarito[8], rodada);
             break;
         case 9:
-            cont = p9(gabarito[8]);
-            break;
-        case 10:
-            cont = p10(gabarito[9]);
+            cont = p1(gabarito[9], rodada);
             break;
         }
         rodada++;
-    } while (cont == 0 || rodada <= 10);
+    } while (cont == 0 || rodada <= 9);
 }
 void mensrodada(int esprodada)
 {
@@ -120,20 +121,52 @@ void certo()
 {
     printf("\t\t\t\tResposta correta!\n\t\t\t\tAguarde para prosseguir\n");
 }
-int p1(char gaba);
+int p1(char gaba, int x)
 {
+    char perguntas[11][180] = {
+        "1- Qual é a finalidade do exercício \"rosca martelo\"?",
+        "2- Qual alimento é responsável por promover a vaso-dilatação nos treinos, reduzindo a pressão arterial, e elevando a taxa de circulação sanguínea, irrigando os músculos com mais eficiência durante os treinos?",
+        "3- Qual alimento é reconhecido por retirar as câimbras de pessoas, pela sua rica fonte de potássio, promovendo a infiltração de sódio nos tecidos musculares, auxiliando na contração e recuperação muscular?",
+        "4- O que significa \"PR\" em musculação?",
+        "5- Qual termo é usado para descrever o processo de redução de gordura corporal para destacar a definição muscular?",
+        "6- Qual é a técnica em que você realiza esforço na fase excêntrica do exercício, na falha muscular?",
+        "7- Qual é o papel das gorduras na dieta de um praticante de musculação?",
+        "8- Qual é a importância da periodização no treinamento de musculação?",
+        "9- Professor é Fake-Natty?",
+        "10- O que é o \"overtraining\" e por que é importante evitá-lo?";
     char r;
+    int i;
+    // int x=0;//posisao do vetor da pergunta;(usar como parâmetro em chamarpergunta)
     do
     {
-        printf("1- Qual é a finalidade do exercício rosca martelo?\n");
+        printf("%s", perguntas[x]);
+        for (i = 0; i < 4; i++)
+        {
+            printf("%s\n", perg[x][i]);
+        }
+        // funcao das alternativas
         scanf("%c", &r);
         stdin;
         r = toupper(r);
-    } while (r != 'A' && r != 'B' && r != 'C' && r != 'D'); // alterar a condição no momento que for implementado ajudas.
-    if (r == /*x*/)
+        if (r != 'A' || r != 'B' || r != 'C' || r != 'D')
+        {
+            printf("Você digitou uma opção inválida, insira novamente a alternativa em :");
+            for (i = 3; i > 0; i--)
+            {
+                if (i == 3)
+                {
+                    Sleep(3000);
+                }
+                printf("%i   ", i);
+                Sleep(1000);
+            }
+            system("cls");
+        }
+    } while (r != 'A' || r != 'B' || r != 'C' || r != 'D'); // alterar a condição no momento que for implementado ajudas.
+    if (r == gaba)
     {
         certo();
-        printf("Premio da rodada:1 Hiper calórico.\n");
+        printf("Premio da rodada: 1 hiper calor\n");//usar vetor para os premios tentar local ou global
         sleep(2000);
         system("cls");
         return 0;
@@ -145,7 +178,7 @@ int p1(char gaba);
         return 1;
     }
 }
-int p2(char *, int *);
+/*int p2(char *, int *);
 {
     char r;
     do
@@ -161,46 +194,47 @@ int p2(char *, int *);
         system ("cls");
         sleep (2000);
         return=0;
-}
-void preencher (){
-strcpy(perg[0][0], "A) Trabalhar de maneira isolada o biceps.");
-strcpy(perg[0][1], "B) Dar enfase na porcao  do biceps braquial, com sinergismo(movimento auxiliar) do ante-braço.");
-strcpy(perg[0][2], "C) Como o exercicio e realizado em uma pegada neutra, trabalha isoladamente a cabeça curta do biceps.");
-strcpy(perg[0][3], "D) trabalhar de maneira isolada o ante-braço.");
-strcpy(perg[1][0], "A)Cenoura.");
-strcpy(perg[1][1], "B)Beterraba.");
-strcpy(perg[1][2], "C)Melancia.");
-strcpy(perg[1][3], "D)Batata-Doce.");
-strcpy(perg[2][0], "A)Ovo.");
-strcpy(perg[2][1], "B)Castanha-do-Pará.");
-strcpy(perg[2][2], "C)Banana.");
-strcpy(perg[2][3], "D)Leite.");
-strcpy(perg[3][0], "A)Progresso Rápido.");
-strcpy(perg[3][1], "B)Peso Real.");
-strcpy(perg[3][2], "C)Potência Reversa.");
-strcpy(perg[3][3], "D)Personal Record (Recorde Pessoal).");
-strcpy(perg[4][0], "A)Hipertrofia.");
-strcpy(perg[4][1], "B)Bulking.");
-strcpy(perg[4][2], "C)Cutting.");
-strcpy(perg[4][3], "D)Endurance.");
-strcpy(perg[5][0], "A)Isometria.");
-strcpy(perg[5][1], "B)Plyometria.");
-strcpy(perg[5][2], "C)Repetições parciais");
-strcpy(perg[5][3], "D)Série negativa");
-strcpy(perg[6][0], "A)Acelerar o metabolismo.");
-strcpy(perg[6][1], "B)Fornecer fibras para a digestão.");
-strcpy(perg[6][2], "C)Apoiar o crescimento dos ossos.");
-strcpy(perg[6][3], "D)Regular hormônios e fornecer energia.");
-strcpy(perg[7][0], "A)Reduzir a necessidade de aquecimento.");
-strcpy(perg[7][1], "B)Evitar o ganho excessivo de massa muscular.");
-strcpy(perg[7][2], "C)Prevenir lesões articulares.");
-strcpy(perg[7][3], "D)Maximizar os ganhos de força e massa muscular.");
-strcpy(perg[8][0], "A)Sim.");
-strcpy(perg[8][1], "B)Com certeza.");
-strcpy(perg[8][2], "C)Sem dúvidas.");
-strcpy(perg[8][3], "D)Inquestionavelmente.");
-strcpy(perg[9][0], "A)Um estado de relaxamento muscular excessivo.");
-strcpy(perg[9][1], "B)Um período de tempo dedicado exclusivamente à recuperação.");
-strcpy(perg[9][2], "C)Estresse corporal de treinamento muito alto sem descanso adequado, o que pode levar a fadiga crônica, lesões e redução no desempenho.");
-strcpy(perg[9][3], "D)Uma técnica avançada de treinamento para maximizar ganhos de força rapidamente.");
+}*/
+void preencher()
+{
+    strcpy(perg[0][0], "A) Trabalhar de maneira isolada o biceps.");
+    strcpy(perg[0][1], "B) Dar enfase na porcao  do biceps braquial, com sinergismo(movimento auxiliar) do ante-braço.");
+    strcpy(perg[0][2], "C) Como o exercicio e realizado em uma pegada neutra, trabalha isoladamente a cabeça curta do biceps.");
+    strcpy(perg[0][3], "D) trabalhar de maneira isolada o ante-braço.");
+    strcpy(perg[1][0], "A)Cenoura.");
+    strcpy(perg[1][1], "B)Beterraba.");
+    strcpy(perg[1][2], "C)Melancia.");
+    strcpy(perg[1][3], "D)Batata-Doce.");
+    strcpy(perg[2][0], "A)Ovo.");
+    strcpy(perg[2][1], "B)Castanha-do-Pará.");
+    strcpy(perg[2][2], "C)Banana.");
+    strcpy(perg[2][3], "D)Leite.");
+    strcpy(perg[3][0], "A)Progresso Rápido.");
+    strcpy(perg[3][1], "B)Peso Real.");
+    strcpy(perg[3][2], "C)Potência Reversa.");
+    strcpy(perg[3][3], "D)Personal Record (Recorde Pessoal).");
+    strcpy(perg[4][0], "A)Hipertrofia.");
+    strcpy(perg[4][1], "B)Bulking.");
+    strcpy(perg[4][2], "C)Cutting.");
+    strcpy(perg[4][3], "D)Endurance.");
+    strcpy(perg[5][0], "A)Isometria.");
+    strcpy(perg[5][1], "B)Plyometria.");
+    strcpy(perg[5][2], "C)Repetições parciais");
+    strcpy(perg[5][3], "D)Série negativa");
+    strcpy(perg[6][0], "A)Acelerar o metabolismo.");
+    strcpy(perg[6][1], "B)Fornecer fibras para a digestão.");
+    strcpy(perg[6][2], "C)Apoiar o crescimento dos ossos.");
+    strcpy(perg[6][3], "D)Regular hormônios e fornecer energia.");
+    strcpy(perg[7][0], "A)Reduzir a necessidade de aquecimento.");
+    strcpy(perg[7][1], "B)Evitar o ganho excessivo de massa muscular.");
+    strcpy(perg[7][2], "C)Prevenir lesões articulares.");
+    strcpy(perg[7][3], "D)Maximizar os ganhos de força e massa muscular.");
+    strcpy(perg[8][0], "A)Sim.");
+    strcpy(perg[8][1], "B)Com certeza.");
+    strcpy(perg[8][2], "C)Sem dúvidas.");
+    strcpy(perg[8][3], "D)Inquestionavelmente.");
+    strcpy(perg[9][0], "A)Um estado de relaxamento muscular excessivo.");
+    strcpy(perg[9][1], "B)Um período de tempo dedicado exclusivamente à recuperação.");
+    strcpy(perg[9][2], "C)Estresse corporal de treinamento muito alto sem descanso adequado, o que pode levar a fadiga crônica, lesões e redução no desempenho.");
+    strcpy(perg[9][3], "D)Uma técnica avançada de treinamento para maximizar ganhos de força rapidamente.");
 }
--- exercícios-lab1 em .c/ponteiros.c/jogo/teste do gabarito.c
+++ exercícios-lab1 em .c/ponteiros.c/jogo/teste do gabarito.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-int gab(char espaba);
-int main (){
-    char gabarito[4]="abc";
-    int x;
-    x= gab(gabarito[1]);
-    printf ("retorno : %i\n", x);
+#include <windows.h>
+int gab(char espaba, int espy);
+int main()
+{
+    char gabarito[4] = "abc";
+    int x, i, y = 5;
+    x = gab(gabarito[2], y);
+    printf("retorno : %i\n", x);
+  printf("Você digitou uma opção inválida, insira novamente a alternativa em :");
+    for (i = 3; i > 0; i--)
+    {
+        if (i==3){
+            Sleep(3000);
+        }
+        printf("%i   ", i);
+        Sleep(1000);
+    }
+    system("cls");  
+    printf ("deu certo\n");
     system ("pause");
     return 0;
 }
-int gab(char espaba){
-    printf ("letra: %c\n", espaba);
+int gab(char espaba, int espy)
+{
+    printf("letra: %c\n", espaba);
+    printf("valor do espy= %d\n", espy);
     return 0;
 }
