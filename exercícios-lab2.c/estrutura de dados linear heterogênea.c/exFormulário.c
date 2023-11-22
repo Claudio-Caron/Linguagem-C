1f7a1354621edb78e13a56dfc158a6c0bb47a162
Claudio André Ferreria Caron <140202178+Claudio-Caron@users.noreply.github.com>
2023-11-22 13:22:54 -0400

    up
    atualização no exercício

--- exercícios-lab2.c/estrutura de dados linear heterogênea.c/exFormulário.c
+++ exercícios-lab2.c/estrutura de dados linear heterogênea.c/exFormulário.c
@@ -10,7 +10,6 @@ typedef struct pront
 {
     float notas[2];
     char aluno[N];
-    int index;
 } pront;
 void inicializar_index(pront *n, int i);
 void incluir(pront *n, int j);
@@ -25,17 +24,24 @@ int main(void)
     {
         i--;
         printf("\tEscolha uma das opções de formulário:\n");
-        printf("\t\t\t[1]- INCLUIR PRONTUÁRIO\n\t\t\t[2]-IMPRIMIR ALUNOS EM ORDEM DE PRONTUÁRIO\n\t\t\t[3]-PESQUISAR PRONTUÁRIO\n\t\t\t[4]-ALTERAR PRONTUÁRIO\n\t\t\t[5]-EXCLUIR PRONTUÁRIO\n\t\t\t[6]-SAIR\n\n\t\t\t\t");
-        scanf("%d", &op);
+        printf("\t\t\t[1]-INCLUIR PRONTUÁRIO\n\t\t\t[2]-IMPRIMIR ALUNOS EM ORDEM DE PRONTUÁRIO\n\t\t\t[3]-PESQUISAR PRONTUÁRIO\n\t\t\t[4]-ALTERAR PRONTUÁRIO\n\t\t\t[5]-EXCLUIR PRONTUÁRIO\n\t\t\t[6]-SAIR\n\n\t\t\t\t");
+        fflush (stdin); scanf("%d", &op);
         system ("cls");
         switch (op)
         {
         case 1:
             dados = malloc(sizeof(pront));
+            if (dados==NULL){
+                system ("cls");
+                printf ("ERRO DE ALOCAMENTO\n");
+                return 1;
+            }
             j++;
             incluir(dados, j);
+            break;
         case 2:
             imprimir (dados, j);
+            break;
         case 3:
 
         case 4:
@@ -47,42 +53,46 @@ int main(void)
             break;
         default:
             printf("INSIRA UMA OPÇÃO VÁLIDA\n");
+            break;
         }
     }
+    free(dados);
+    return 0;
 }
-void inicializar_index(pront *n, int i)
+/*void inicializar_index(pront *n, int i)
 {
     n[i].index = 0;
-}
+}*/
 void incluir(pront *n, int j)
 {
     float nota1, nota2;
-    system("cls");
     printf("\t\t\tInsira o nome do aluno:\n");
-    scanf("%s", n[j].aluno);
+    fflush (stdin); fgets (n[j].aluno, sizeof(n[j].aluno), stdin);
     do
     {
         printf("\t\t\tInsira a primeira nota:\n");
-        scanf("%f", &nota1);
+        fflush (stdin); scanf("%f", &nota1);
         printf("\t\t\tInsira  a segunda nota:\n");
-        scanf("%f", &nota2);
-        if (nota1 < 0 || nota1 > 10 || nota1 < 0 || nota2 > 10)
+        fflush (stdin); scanf("%f", &nota2);
+        if (nota1 < 0 || nota1 > 10 || nota2 < 0 || nota2 > 10)
         {
             system("cls");
             printf("\t\t\tINSIRA UM VALOR DENTRO DA FAIXA(0 a 10)\n");
+            printf ("\t\t\taluno: %s\n", n[j].aluno);
         }
-    } while (nota1 < 0 || nota1 > 10 || nota1 < 0 || nota2 > 10);
+    } while (nota1 < 0 || nota1 > 10 || nota2 < 0 || nota2 > 10);
     n[j].notas[0] = nota1;
     n[j].notas[1] = nota2;
+    system ("cls");
 }
 void imprimir(pront *p, int j){
     int i;
     system ("cls");
     for (i=0; i<=j; i++){
-        printf ("\t\t\t--------------------\n");
-        printf ("\t\t\t|nome: %s           |\n", p[i].aluno);
-        printf ("\t\t\tNota 1: %.2f        |\n", p[i].notas[0]);
-        printf ("\t\t\tNota 2: %.2f        |\n", p[i].notas[1]);
-        printf ("\t\t\t---------------------\n");
+        printf ("\t\t\t----------------------------\n");
+        printf ("\t\t\t|Nome: %s                   |\n", p[i].aluno);
+        printf ("\t\t\t|Nota 1: %.2f               |\n", p[i].notas[0]);
+        printf ("\t\t\t|Nota 2: %.2f               |\n", p[i].notas[1]);
+        printf ("\t\t\t-----------------------------\n\n");
     }
 }
