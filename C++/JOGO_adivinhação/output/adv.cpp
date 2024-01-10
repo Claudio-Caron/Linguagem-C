3792c920bcefc1934ed213d47ec8c2840f52b8af
Claudio André Ferreria Caron <140202178+Claudio-Caron@users.noreply.github.com>
2024-01-10 11:18:50 -0400

    conserto no segmento de apredizado de cpp

--- C++/JOGO_adivinhação/output/adv.cpp
+++ C++/JOGO_adivinhação/output/adv.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
 using namespace std;
 int main (){
-
-    int chute, certo=42, tentativas=3;
-    bool verifica=chute==certo;
-    bool menor=chute<certo;
+    int chute, certo, tentativas=3;
+    certo=42;
+    bool maior, verifica;
     cout<<"*************************"<< endl;
     cout<<"***JOGO DA ADIVINHACAO***"<< endl;
     cout<<"*************************"<< endl;
     while(tentativas){
-    cout<<"TENTATIVAS RESTANTES: "<< tentativas <<"\n Insira um valor para chute"<< endl;
-    cin>>chute;
-    if (menor){
-        cout<< "O Numero digitado e menor que o numero secreto"<<endl;
-        tentativas--;
-    }else if(verifica){
-        cout<<"PARABENS!!!Voce acertou o numero secreto:/"<<endl;
-    }else{
-        cout<<"O numero inserido e maior que o procurado"<<endl;
-        tentativas--;
-    }
+        cout<<"TENTATIVAS RESTANTES: "<< tentativas <<"\n Insira um valor para chute \n";
+        cin>>chute;
+        maior=chute>certo;
+        verifica=chute==certo;
+        if (maior){
+            cout<< "O Numero digitado e maior que o numero secreto"<<endl;
+            tentativas--;
+        }else if(verifica){
+                cout<<"PARABENS!!!Voce acertou o numero secreto:/"<<endl;
+        }else{
+                cout<<"O numero inserido e menor que o procurado"<<endl;
+                tentativas--;
+        }
 
-}
+
+    }
 }
