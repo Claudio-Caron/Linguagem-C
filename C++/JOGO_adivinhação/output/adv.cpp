2fa99bf65d0942f35dd90ea7be887eb0e9dad905
Claudio André Ferreria Caron <140202178+Claudio-Caron@users.noreply.github.com>
2024-01-11 14:24:37 -0400

    adicionado funcionalidades e manipulação de ponto flutuantes em cpp

--- C++/JOGO_adivinhação/output/adv.cpp
+++ C++/JOGO_adivinhação/output/adv.cpp
@@ -2,14 +2,38 @@
 using namespace std;
 int main()
 {
-    int chute, certo, tentativas = 3;
+    int chute, certo, cont=0, tentativas;
+    char difi;
     certo = 42;
+    cout.precision(2);
+    cout<<fixed;
     bool maior, verifica;
-    cout << "*************************" << endl;
-    cout << "***JOGO DA ADIVINHACAO***" << endl;
-    cout << "*************************" << endl;
+    cout <<"*************************" << endl;
+    cout <<"***JOGO DA ADIVINHACAO***" << endl;
+    cout <<"*************************" << endl;
+    cout <<" ESCOLHA UM DIFICULDADE PARA INICIAR O JOGO\n(dificuldade baseada em quantidade de tentativas)"<<endl;
+    cout <<"_____________________________________" << endl;
+    cout <<"|Facil(F)  |  Medio(M)  |  Dificil  |"<<endl;
+    cout <<"-------------------------------------"<< endl;
+    cin >>difi;
+    switch(difi){
+    case 'F':
+        tentativas=10;
+        break;
+    case'M':
+        tentativas=5;
+        break;
+    case 'D':
+        tentativas=3;
+        break;
+    default:
+        cout<< "Voce nao escolheu uma dificuldade valida!! acionado modo facil"<<endl;
+        tentativas=10;
+        break;
+    }
     while (tentativas)
     {
+        cont++;
         cout << "TENTATIVAS RESTANTES: " << tentativas << "\n Insira um valor para chute \n";
         cin >> chute;
         maior = chute > certo;
@@ -20,7 +44,8 @@ int main()
         }
         else if (verifica)
         {
-            cout << "PARABENS!!!Voce acertou o numero secreto:/" << endl;
+            cout << "PARABENS!!!Voce acertou o numero secreto com " << cont<<" tentativas"<< endl;
+            break;
         }
         else
         {
