6bff8dde02c3be37420265971cba7b18efa676ee
Claudio André Ferreria Caron <140202178+Claudio-Caron@users.noreply.github.com>
2024-01-11 09:56:38 -0400

    Update adv.cpp

--- C++/JOGO_adivinhação/output/adv.cpp
+++ C++/JOGO_adivinhação/output/adv.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
 using namespace std;
-int main (){
-    int chute, certo, tentativas=3;
-    certo=42;
+int main()
+{
+    int chute, certo, tentativas = 3;
+    certo = 42;
     bool maior, verifica;
-    cout<<"*************************"<< endl;
-    cout<<"***JOGO DA ADIVINHACAO***"<< endl;
-    cout<<"*************************"<< endl;
-    while(tentativas){
-        cout<<"TENTATIVAS RESTANTES: "<< tentativas <<"\n Insira um valor para chute \n";
-        cin>>chute;
-        maior=chute>certo;
-        verifica=chute==certo;
-        if (maior){
-            cout<< "O Numero digitado e maior que o numero secreto"<<endl;
-            tentativas--;
-        }else if(verifica){
-                cout<<"PARABENS!!!Voce acertou o numero secreto:/"<<endl;
-        }else{
-                cout<<"O numero inserido e menor que o procurado"<<endl;
-                tentativas--;
+    cout << "*************************" << endl;
+    cout << "***JOGO DA ADIVINHACAO***" << endl;
+    cout << "*************************" << endl;
+    while (tentativas)
+    {
+        cout << "TENTATIVAS RESTANTES: " << tentativas << "\n Insira um valor para chute \n";
+        cin >> chute;
+        maior = chute > certo;
+        verifica = chute == certo;
+        if (maior)
+        {
+            cout << "O Numero digitado e maior que o numero secreto" << endl;
         }
-
-
+        else if (verifica)
+        {
+            cout << "PARABENS!!!Voce acertou o numero secreto:/" << endl;
+        }
+        else
+        {
+            cout << "O numero inserido e menor que o procurado" << endl;
+        }
+        tentativas--;
     }
 }
