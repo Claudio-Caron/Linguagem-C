0c08a174c2773bde23a0ca094cb47a8a2466bbe3
Claudio André Ferreria Caron <140202178+Claudio-Caron@users.noreply.github.com>
2024-06-07 10:38:16 -0400

    Create MapeamentoMain.cpp

---
+++ C++/JOGO_adivinhação/TrabalhoArquitetura/MapeamentoMain.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdlib.h>
+#include <windows.h>
+#include <fstream>
+using namespace std;
+void transformar ();
+void PainelDeAbertura();
+void InicializarMemórias(Cache cache,MP mp);
+
+
+
+
+ int main(int argc, char const *argv[])
+ {
+    int x=0;
+    ofstream arquivo;
+    /* code */
+    return 0;
+ }
+ void PainelDeAbertura(){
+    cout << "Trabalho De Aquitetura"  endl;
+    cout << "Pressione uma tecla para continuar: \n";
+
+    sistem ("cls");
+ }
+ 
